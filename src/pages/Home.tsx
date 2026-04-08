4f8ed50e04c63250bbcce9f0b0050b4942ea3850
Lucas Oliveira <lucasoliveira96@outlook.com>
2026-04-08 10:59:47 -0400

    Change image class from 'object-cover' to 'object-contain'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -244,7 +244,7 @@ function AnimalCard({ animal, photoUrl, procedures, onHelp }: {
           <img
             src={cloudinaryUrl(photoUrl, 'w_400,h_176,c_fill,q_auto,f_auto')}
             alt={animal.name}
-            className="w-full h-full object-cover"
+            className="w-full h-full object-contain"
           />
         ) : isAdopted ? (
           <div className="w-full h-full flex flex-col items-center justify-center gap-2">
